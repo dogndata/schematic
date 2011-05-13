e619711d5a871b2564f19ade073a6994a53136d1
Dave Goddard & Michael Schubert <pair+dgoddard+mschubert@pivotallabs.com>
2011-05-13 10:02:08 -0400

    use autoload for all schematic files

--- lib/schematic.rb
+++ lib/schematic.rb
@@ -4,15 +4,30 @@ module Schematic
       "This class does not include ActiveModel. You cannot generate an XSD from it."
     end
   end
+  module Generator
+    autoload :Xsd, 'schematic/generator/xsd'
+    autoload :Names, 'schematic/generator/names'
+    autoload :Namespaces, 'schematic/generator/namespaces'
+    autoload :Column, 'schematic/generator/column'
+    autoload :Types, 'schematic/generator/types'
+
+    module Restrictions
+      autoload :Base, 'schematic/generator/restrictions/base'
+      autoload :Custom, 'schematic/generator/restrictions/custom'
+      autoload :Enumeration, 'schematic/generator/restrictions/enumeration'
+      autoload :Length, 'schematic/generator/restrictions/length'
+      autoload :Pattern, 'schematic/generator/restrictions/pattern'
+    end
+  end
+  module Serializers
+    autoload :Xsd, 'schematic/serializers/xsd'
+  end
+
+  autoload :Version, 'schematic/version'
 end
 
 require "builder"
-
 require 'active_support/inflector/inflections'
 require 'active_support/inflections'
-
-Dir[File.join(File.dirname(__FILE__), "schematic/**/*.rb")].each do |file|
-  require file.gsub(/\/.rb$/,'')
-end
 
 ActiveRecord::Base.send(:extend, Schematic::Serializers::Xsd)
