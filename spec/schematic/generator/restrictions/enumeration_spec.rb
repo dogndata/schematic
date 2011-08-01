6d6e9b73f89b21c465ab8733412c785bba5da0fa
Michael Schubert <pair+schubert@pivotallabs.com>
2011-08-01 13:37:20 -0400

    add documentation, more test cases and bump version

--- spec/schematic/generator/restrictions/enumeration_spec.rb
+++ spec/schematic/generator/restrictions/enumeration_spec.rb
@@ -10,18 +10,22 @@ describe Schematic::Generator::Restrictions::Enumeration do
           t.string "title"
           t.string "should_be_skipped"
           t.string "should_also_be_skipped"
+          t.string "and_also_be_skipped"
           t.boolean "active"
           t.string "options"
           t.integer "force_enumeration"
+          t.integer "skip_enumeration"
         end
 
         model do
           validates :title, :inclusion => { :in => ["a", "b", "c"] }
           validates :should_be_skipped, :inclusion => ["a", "b", "c"], :if => lambda { false }
           validates :should_also_be_skipped, :inclusion => ["a", "b", "c"], :unless => lambda { false }
+          validates :and_also_be_skipped, :inclusion => ["a", "b", "c"], :if => lambda { true}, :unless => lambda { false }
           validates :active, :inclusion => { :in => [true, false] }
           validates :options, :inclusion => { :in => lambda { |f| ["some valid attribute"] } }
           validates :force_enumeration, :inclusion => { :in => [1, 2], :xsd => { :include => true} }, :if => lambda { false }
+          validates :skip_enumeration, :inclusion => { :in => [1, 2], :xsd => { :include => false} }, :if => lambda { true }
         end
       end
 
@@ -31,7 +35,14 @@ describe Schematic::Generator::Restrictions::Enumeration do
         lambda {
           validate_xml_against_xsd(xml, subject)
         }.should raise_error
-        valid_instance = EnumerationModel.new(:title => "a", :should_be_skipped => "a", :should_also_be_skipped => "a", :active => true, :options => "some valid attribute", :force_enumeration => 2)
+        valid_instance = EnumerationModel.new(:title => "a",
+                                              :should_be_skipped => "a",
+                                              :should_also_be_skipped => "a",
+                                              :and_also_be_skipped => "a",
+                                              :active => true,
+                                              :options => "some valid attribute",
+                                              :force_enumeration => 2,
+                                              :skip_enumeration => 2)
         xml = [valid_instance].to_xml
         lambda {
           validate_xml_against_xsd(xml, subject)
@@ -68,6 +79,14 @@ describe Schematic::Generator::Restrictions::Enumeration do
                   </xs:simpleContent>
                 </xs:complexType>
               </xs:element>
+              <xs:element name="and-also-be-skipped" minOccurs="0" maxOccurs="1">
+                <xs:complexType>
+                  <xs:simpleContent>
+                    <xs:restriction base="String">
+                    </xs:restriction>
+                  </xs:simpleContent>
+                </xs:complexType>
+              </xs:element>
               <xs:element name="active" minOccurs="0" maxOccurs="1">
                 <xs:complexType>
                   <xs:simpleContent>
@@ -90,6 +109,14 @@ describe Schematic::Generator::Restrictions::Enumeration do
                     <xs:restriction base="Integer">
                       <xs:enumeration value="1"/>
                       <xs:enumeration value="2"/>
+                    </xs:restriction>
+                  </xs:simpleContent>
+                </xs:complexType>
+              </xs:element>
+              <xs:element name="skip-enumeration" minOccurs="0" maxOccurs="1">
+                <xs:complexType>
+                  <xs:simpleContent>
+                    <xs:restriction base="Integer">
                     </xs:restriction>
                   </xs:simpleContent>
                 </xs:complexType>
